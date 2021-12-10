c4ca6f00cb2d5730992ca7396fc76ee2c3e95c9a
ydjabela <ydjabela@blendbow.com>
2021-12-10 17:46:48 +0100

    add class player

---
+++ player.py
@@ -0,0 +1,53 @@
+from tinydb import TinyDB
+import json
+import settings
+
+# ---------------------------------------------------------------------------------------------------------------------#
+
+
+class Player:
+
+    def __init__(self, familly_name=None, first_name=None, age=None, sex=None, classement=None):
+        self.familly_name = familly_name
+        self.first_name = first_name
+        self.age = age
+        self.sex = sex
+        self.classement = classement
+
+    # convertir en dictionnaire$
+    def serialzation(self, player):
+        serialized_player = {
+            'familly_name': player.familly_name,
+            'first_name': player.first_name,
+            'age': player.age,
+            'sex': player.sex,
+            'classement': player.classement
+        }
+        return serialized_player
+
+    def add_player(self):
+
+        familly_name = str(input('familly_name : '))
+        first_name = str(input('first_name: '))
+        age = int(input('age: '))
+        sex = str(input('sex: '))
+        classement = int(input('classement: '))
+        player = Player(familly_name=familly_name, first_name=first_name, age=age, sex=sex, classement=classement)
+        serialized_players = self.serialzation(player=player)
+        return serialized_players
+
+    def add_players(self):
+        players = list()
+        nbr_player = settings.nbr_player
+        for i in range(1, nbr_player+1):
+            print('add player number : ', i)
+            serialized_players = self.add_player()
+            players.append(serialized_players)
+
+        db = TinyDB('db.json')
+        players_table = db.table('players')
+        # clear the table first
+        players_table.truncate()
+        for player in players:
+            players_table.insert(player)
+
