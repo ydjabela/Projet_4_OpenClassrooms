31e229c36d390f0591381694230de71d3200d5d4
ydjabela <ydjabela@blendbow.com>
2021-12-11 00:45:14 +0100

    add choice menu

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from player import Player
 from tournament import Tournament
 
+class choice:
 
-Tournament().add_tournament()
-Player().add_players()
+    def main_choice():
+        print()
+        print("Bienvenue sur le gestionnaire de jeu d'échec.\n")
+        print("Selectionnez le menu souhaité.\n")
+        print(" 1 : ajouter un joueur.")
+        print(" 2 : création d'un tournoi.")
+        print(" 3 : sortir du logiciel.")
+        print("\nQuelle est votre choix : ")
+        resultat = input()
+        return resultat
+
+
+resultat = int(choice.main_choice())
+if resultat == 1:
+    Player().add_players()
+elif resultat == 2:
+    Tournament().add_tournament()
+elif resultat == 3:
+    exit()
+else:
+    resultat = choice.main_choice()
