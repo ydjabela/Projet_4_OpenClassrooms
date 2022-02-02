bdcbd8bdacd7696d84dc87f3c009e709b9f163fe
ydjabela <ydjabela@blendbow.com>
2022-02-02 13:05:52 +0100

    creat view

---
+++ view.py
@@ -0,0 +1,25 @@
+
+# ---------------------------------------------------------------------------------------------------------------------#
+
+
+class Choice:
+
+    def main_choice(self):
+        print()
+        print("Bienvenue sur le gestionnaire de jeu d'échec.\n")
+        print("Selectionnez le menu souhaité.\n")
+        print(" 1 : ajouter les joueurs.")
+        print(" 2 : création d'un tournoi.")
+        print(" 3 : sortir du logiciel.")
+        print("\nQuelle est votre choix : ")
+        resultat = input()
+        return resultat
+
+    # ---------------------------------------------------------------------------------------------------------------------#
+
+    def print_error_enter_int(self):
+        # indicates to the user that he must enter a number
+
+        print("\n ERREUR : vous devez entrer un chiffre correspondant à votre choix .")
+
+    # ---------------------------------------------------------------------------------------------------------------------#
